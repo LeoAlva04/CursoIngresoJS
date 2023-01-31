5bf74a7d93dea26ed8fc5e0cf744a325d8574722
Leonardo Alvarez <leosebalv12@gmail.com>
2023-01-31 16:50:37 -0300

    30/01

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -4,6 +4,10 @@ se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
 Alvarez leonardo Sebastian Div X */
 function mostrar()
 {
-	alert("Esto funciona de maravilla"); 	// alert: funciona  para mostrar un mensaje emergente, siempre en minuscula, xq sino no lo reconoce 
-}											// El ambito de las variables o scope es elespacio entre las llaves ({...}) 
+	
+	// alert: funciona  para mostrar un mensaje emergente, siempre en minuscula, xq sino no lo reconoce 
 
+	alert("Esto funciona de maravilla"); 	
+
+}											
+// El ambito de las variables o scope es elespacio entre las llaves ({...}) 
