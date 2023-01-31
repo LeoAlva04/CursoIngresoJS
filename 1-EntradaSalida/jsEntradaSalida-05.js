96b9b3b37b859a880921955ec54c14b699f24c46
Leonardo Alvarez <leosebalv12@gmail.com>
2023-01-31 18:19:52 -0300

    h1819f3101

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,30 @@
 /*
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
-ej.: "Usted se llama José y tiene 66 años" 	*/
+ej.: "Usted se llama José y tiene 66 años"
+Alvarez Leonardo Sebastian Div X
+*/
 function mostrar()
 {	
-	alert("ok");
+	//declare la variable nombre 
+	let nombre;
+
+	//declare la variable edad
+	let edad;
+
+	//guardo lo escrito en la variable nombre
+	nombre = document.getElementById("txtIdNombre").value;	
+
+	//guardo lo escrito en la variable Edad
+	edad = document.getElementById("txtIdEdad").value;
+	
+	//lo coloco en alert para mostrarlo y uso la tecla (+) para concatenar
+	
+	alert("Su nombre es: " + nombre + " y tiene " +  edad + " años");
+	
+	
+	/*_alert(`su nombre es: ${nombre} y tiene  ${edad} años `): _*/
+	
+	/*Esta es otra forma para no usar el (+), se usa (`...`) para marcar todo el texto y para marcar la variable
+	se utiliza (${...})*/
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,34 @@
 /*
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
-mostrar el resulto por medio de "ALERT"*/
+mostrar el resulto por medio de "ALERT"
+Alvarez Leonardo Sebastian Div X
+*/
 function sumar()
 {
-	alert("ok");
+	//declaro las variables 
+	let  numeroUno;
+	let  numeroDos;
+	let result;
+
+	//carge el texto que ingresa el ususario a la variable
+	numeroUno = document.getElementById("txtIdNumeroUno").value;
+	numeroDos = document.getElementById("txtIdNumeroDos").value;	
+ 
+	//Asi se paso el texto que ingreso el usuario a numero entero
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	//aca realiza la suma y se carga en la variable result (resultado)
+	result = ( numeroUno + numeroDos );
+
+	//el resultado lo muestro con alert
+	alert(result);
 }
 
+
+
+//parseing: se utiliza para convertir algo de tipo cadena "escrito "(escrito a valor nuemrico (number) y visceversa
+//parseInt: convierte dato cadena a numero entero 
+//parseFloat: convierte dato cadena en flotante a numero, acepta numero decimal 
+//casting: convertir un tipo de dato numerico a otro numerico 
